bb012d91f0d55c94909a767bcd279eb8f6b1c72f
Emma Bäckström <backstromemma@gmail.com>
2016-12-27 19:48:46 +0100

    Check how many mines adjacent to each brick

--- js/model/model.js
+++ js/model/model.js
@@ -1,7 +1,7 @@
 var msModel = function() {
 
 	//hard coded for development
-	var mineNum = 8;
+	var mineNum = 4;
  
 	//list of observers
 	this._observers  = [];
@@ -30,7 +30,8 @@ var msModel = function() {
 		var brick = {
 			mine: false,
 			open: false,
-			minesSurrounding: 0
+			surroundingMines: 0,
+			flagged: false
 		};
 
 		return brick;
@@ -53,7 +54,8 @@ var msModel = function() {
 		}
 
 		this.board = board;
-		this.placeMines(this.board, mineNum);
+		this.placeMines(this.board, mineNum); // future feature: place mines after first click!
+		this.checkSurrounding();
 		callback();
 	};
 
@@ -70,7 +72,7 @@ var msModel = function() {
 				var row = this.getRandomInt(0, totRow-1);
 				var col = this.getRandomInt(0, totCol-1);
 
-				if (board[row][col].mine == false) {
+				if (board[row][col].mine === false) {
 					board[row][col].mine = true;
 					tryPlacingMine = false;
 				}
@@ -86,6 +88,71 @@ var msModel = function() {
 		min = Math.ceil(min);
 		max = Math.floor(max);
 		return Math.floor(Math.random() * (max - min + 1)) + min;
+	};
+
+	this.countSurrounding = function(r, c, rowPos, colPos) {
+		var mineCount = 0;
+		console.log(rowPos, colPos);
+
+		for (var i = 0; i < rowPos.length; i++) {
+			if ( this.board[r+rowPos[i]][c+colPos[i]].mine === true ) {
+				mineCount += 1;
+			}
+		}
+
+		return mineCount;
+	};
+
+	this.checkSurrounding = function() {
+		var board = this.board;
+		var mineCount;
+		var rAround;
+		var cAround;
+
+		for (var r = 0; r < board.length; r++) {
+
+			for (var c = 0; c < board[0].length; c++) {
+
+				if (r === 0) { // first row
+					if (c === 0) {
+						rAround = [0, 1, 1];
+						cAround = [1, 0, 1];
+					} else if (c === board[0].length-1) {
+						rAround = [0, 1, 1];
+						cAround = [-1, -1, 0];
+					} else {
+						rAround = [0, 1, 1, 1, 0];
+						cAround = [-1, -1, 0, 1, 1];
+					}
+				} else if (r === board.length-1) { // last row
+					if (c === 0) {
+						rAround = [-1, -1, 0];
+						cAround = [0, 1, 1];
+					} else if (c === board[0].length-1) {
+						rAround = [0, -1, -1];
+						cAround = [-1, -1, 0];
+					} else {
+						rAround = [0, -1, -1, -1, 0];
+						cAround = [-1, -1, 0, 1, 1];
+					}
+				} else { // the other rows
+					if (c === 0) {
+						rAround = [-1, -1, 0, 1, 1];
+						cAround = [0, 1, 1, 0, 1];
+					} else if (c === board[0].length-1) {
+						rAround = [-1, -1, 0, 1, 1];
+						cAround = [-1, 0, -1, -1, 0];
+					} else {
+						rAround = [-1, -1, -1, 0, 0, 1, 1, 1];
+						cAround = [-1, 0, 1, -1, 1, -1, 0, 1];
+					}
+				}
+
+				mineCount = this.countSurrounding(r, c, rAround, cAround);
+				this.board[r][c].surroundingMines = mineCount;
+			}
+
+		}
 	};
 	
 
